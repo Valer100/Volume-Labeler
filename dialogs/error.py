fc1102fe6181956ebcea5f3ed2c4a7070900a467
Valer100 <122545522+Valer100@users.noreply.github.com>
2025-03-19 17:08:21 +0200

    Fix error dialog not using the right color variables from `custom_ui`

--- dialogs/error.py
+++ dialogs/error.py
@@ -14,10 +14,10 @@ def show(error: str):
     ttk.Label(header, text = "\ue7ba ", font = ("Segoe UI", 17), padding = (0, 5, 0, 0)).pack(side = "left")
     ttk.Label(header, width = 25, text = strings.lang.error, font = ("Segoe UI Semibold", 17)).pack(side = "left")
 
-    error_text = ScrolledText(window, width = 60, height = 7, wrap = "word", background = custom_ui.entry_bg,
-                                 foreground = custom_ui.fg, selectbackground = custom_ui.entry_select,
+    error_text = ScrolledText(window, width = 60, height = 7, wrap = "word", background = custom_ui.colors.entry_bg,
+                                 foreground = custom_ui.colors.fg, selectbackground = custom_ui.colors.entry_select,
                                  selectforeground = "#ffffff", highlightthickness = 1, relief = "solid",
-                                 highlightbackground = custom_ui.entry_bd, highlightcolor = custom_ui.entry_bd,
+                                 highlightbackground = custom_ui.colors.entry_bd, highlightcolor = custom_ui.colors.entry_bd,
                                  border = 0, font = ("Consolas", 10))
     error_text.pack()
     error_text.insert("1.0", error)
@@ -28,10 +28,10 @@ def show(error: str):
         messagebox.showinfo(parent = window, title = strings.lang.copy_traceback, message = strings.lang.copy_traceback_success)
 
     buttons = ttk.Frame(window)
-    buttons.pack(pady = preferences.get_scaled_value(16), anchor = "e")
+    buttons.pack(pady = preferences.get_scaled_value(16), fill = "x")
 
+    custom_ui.Button(buttons, text = strings.lang.copy_traceback, command = copy_traceback).pack(side = "left")
     custom_ui.Button(buttons, text = strings.lang.ok, default = "active", command = window.destroy).pack(side = "right", padx = (preferences.get_scaled_value(8), 0))
-    custom_ui.Button(buttons, text = strings.lang.copy_traceback, command = copy_traceback).pack(side = "right")
 
     window.resizable(False, False)
     window.focus_set()
